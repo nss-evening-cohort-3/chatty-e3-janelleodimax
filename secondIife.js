986bbcc2f939d6154df31048e2d831324822fc15
Max Baldwin <maxrbaldwin@gmail.com>
2016-03-21 20:55:57 -0500

    adding before I forget

--- secondIife.js
+++ secondIife.js
@@ -1,12 +1,50 @@
 "use strict";
+let = var
 
 //variable set by the "use strict" method
-let userInput;
-let userInputField = document.getElementById("text-area");
-let userMessage = document.getElementById("user-message");
-let deleteMessage = document.getElementById("delete-message");
+let chatbox = (function(newChatbox){
+  let userInputField = document.getElementById("text-area");
+  let userMessage = document.getElementById("user-message");
+  let deleteMessage = document.getElementById("delete-message");
+  let inputString;
 
-createButton.addEventListener("click", function(event)) {
-  userInput = document.getElementById("text-area").value;
-  deleteMessage.innerHTML += ""
-}
+//user input here
+userInputField.addEventListener("keydown", function(event) {
+  if (event.keyCode === 13) {
+
+    inputString =`<div>${userInput.value}`;
+    inputString += `<button class = "btn btn-default">Delete</button></div>`;
+    userMessage.innerHTML += inputString;
+    userInputField.value = "";
+  };
+});
+
+deleteMessage.addEventListener("click", function() {
+  userInputField.innerHTML = "";
+})
+
+return newChatbox;
+})(chatbox || {});
+
+//creating the chatbox
+let chatbox = (function(oldChatbox) {
+  oldChatbox.loopThrough = function(messageArray) {
+    let string = "";
+    for(var i = 0; i <messageArray.length; i++) {
+      string += `<div>${messageArray[i].content}`;
+      string += `button class = "btn btn-default">Delete</button></div>`;
+    }
+    document.getElementById("messages").innerHTML = string;
+  }
+  return oldChatbox;
+})
+(chatbox || {});
+
+//delete the messages
+let chatbox = (function (oldChatbox) {
+  oldChatbox.deleteButton = function(event) {
+    this.removeChild(event.target.parentNode);
+  }
+  return oldChatbox;
+})
+(chatbox || {});
