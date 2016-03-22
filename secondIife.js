96a953f232dbe434146d856492e3c8d1790af33f
Max Baldwin <maxrbaldwin@gmail.com>
2016-03-21 19:22:07 -0500

    initial second iife

---
+++ secondIife.js
@@ -0,0 +1,12 @@
+"use strict";
+
+//variable set by the "use strict" method
+let userInput;
+let userInputField = document.getElementById("text-area");
+let userMessage = document.getElementById("user-message");
+let deleteMessage = document.getElementById("delete-message");
+
+createButton.addEventListener("click", function(event)) {
+  userInput = document.getElementById("text-area").value;
+  deleteMessage.innerHTML += ""
+}
