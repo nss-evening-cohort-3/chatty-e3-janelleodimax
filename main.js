e50931e45da19273718c5c53b6a57d928bae0937
Odie Joseph <Odie Joseph>
2016-03-23 02:03:44 -0500

    I have created the dark theme for the chatbox. Had to change the id for the dark theme to just dark bc hyphenated statement messing up code

--- main.js
+++ main.js
@@ -5,3 +5,13 @@ var Chatty = (function() {
   Chatty.loadStarterPack(Chatty.getStarterPack);
     
 })(Chatty || {});
+
+ let box = document.getElementById("chatbox");
+ let check = document.getElementById("dark");
+
+ check.addEventListener("click", function(){
+    if(check.checked)
+    box.classList.add("theme");
+    else if(check.checked === false)
+    box.classList.remove("theme");
+ });
